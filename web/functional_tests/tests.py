2b59f09396a9c1a06f60d7e9afbb1a32f88c68f5
David Thiriez <dvthiriez@gmail.com>
2015-10-30 00:40:57 +0000

    add selenium requirement for web portal FT. FT test.py missed from last commit

--- web/functional_tests/tests.py
+++ web/functional_tests/tests.py
@@ -1,33 +1,10 @@
-# WARNING: Code sourced from python3 implementation
-
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
-
-from datetime import datetime
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
-
-from django.contrib.auth.models import User
-from security.models import JSONWebKey
-from vehicles.models import Vehicle
+from unittest import skip
+from .base import FunctionalTest
 
 
-class FunctionalTest(StaticLiveServerTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-            super().setUpClass()
-            cls.server_url = cls.live_server_url
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
+## TODO complete test on desktop environment for web browser support
+@skip
+class WebPortalTest(FunctionalTest):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -35,68 +12,6 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
-
-    def setup_mock_key(self, username):
-        private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
-            backend=default_backend()
-        )
-        public_key = private_key.public_key()
-        pem = public_key.public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
-        )
-
-        valid_from = datetime.now()
-        valid_to = valid_from.replace(valid_from.year + 1)
-        
-        key = JSONWebKey.objects.create(
-            key_name='key_' + username,
-            key_valid_from=valid_from,
-            key_valid_to=valid_to,
-            key_pem=pem
-        )
-        key.save()
-        return key    
-    
-    def setup_mock_data(self):
-        owner = User.objects.create_user(username='dthiriez',
-                                            password='rvi')
-        owner_key = setup_mock_key(owner.username)
-
-        vehicle = Vehicle.objects.create(
-            veh_name='vehicle_test',
-            veh_key=owner_key,
-            account=owner
-        )
-        vehicle.save()
-
-        guest = User.objects.create_user(
-            username='arodriguez',
-            password='rvi'
-        )
-        
-        guest_key = setup_mock_key(guest.username)
-
-        guest_device = Device.objects.create(
-            dev_name='device_arodriguez',
-            dev_owner='arodriguez',
-            dev_uuid='1234',
-            dev_key=guest_key,
-            dev_rvibasename='jlr.com'
-        )
-
-        valid_from = datetime.now()
-        valid_to = valid_from.replace(valid_from.year + 1)
-        guest_remote = Remote(
-            rem_name='remote_arodriguez',
-            rem_vehicle=vehicle,
-            rem_device=guest_device,
-            rem_validfrom=valid_from,
-            rem_validto=valid_to
-        )
-
 
     def test_can_access_and_setup_rvi_services(self):
         # David has has heard about a cool new open source telematics project.
@@ -114,9 +29,46 @@ class FunctionalTest(StaticLiveServerTestCase):
 
         # To start using rvi, David proceed to set up accounts, security keys, 
         # devices, remotes, and a vehicle for an owner and guest user
-        self.setup_mock_data()
 
         # David returns to the web site to check the owner portal
         self.fail('Finish the test!')
 
+
+class RviTest(FunctionalTest):
+    
+    def test_data_setup(self):
+        owner = self.setup_user(username='dthiriez',password='rvi')
+        owner_key = self.setup_key(owner)
+        vehicle = self.setup_vehicle(owner, owner_key)
+
+        guest = self.setup_user(username='arodriguez',password='rvi')
+        guest_key = self.setup_key(guest)
+        guest_device = self.setup_device(guest, guest_key)
+        guest_remote = self.setup_remote(guest, guest_device, vehicle)
+
+    def test_can_send_remote(self):
+        # David proceeds to set up an account, security key, device, and 
+        # vehicle in order to be able to send a remote
         
+        # Simulating the smart phone app, David triggers a 'cert_create' to
+        # RVI Service hosted by the backend.
+
+        # He checks the backend data to confirm the 'cert_create' call
+        # registered and updated the database with the new Remote
+
+        # Next he confirm that the rvi server logs show the new Remote
+        # was passed on to rvi_core.
+
+        # The Remote should now be queued up by rvi_core and ready to be
+        # sent as soon as the device comes online
+
+        # David then simulates the device coming online by hosting its
+        # 'cert_provision' service available on the rvi network
+
+        # To confirm the data was received, he check that the Remote he
+        # sent was received by the 'cert_provision' service
+        self.fail('Finish the test!')
+
+    def test_can_receive_service_history_record(self):
+        self.fail('Finish the test')
+    
