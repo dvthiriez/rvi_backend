459b32de216302843f88624070b06d58eedc6c9e
David Thiriez <dvthiriez@gmail.com>
2015-11-13 00:48:09 +0000

    [dev-deploytools] automate additional provisioning tasks in fabfile.py

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -3,9 +3,10 @@ from fabric.api import env, local, run, warn_only
 from fabric.context_managers import shell_env
 import random
 
-
-BACKEND_REPO_URL = 'https://github.com/dvthiriez/rvi_backend.git'
-CORE_REPO_URL = 'https://github.com/dvthiriez/rvi_core.git'
+GIT_USERNAME = ''
+GIT_PASSWORD = ''
+BACKEND_REPO_URL = 'https://%s:%s@github.com/dvthiriez/rvi_backend.git' % (GIT_USERNAME, GIT_PASSWORD)
+CORE_REPO_URL = 'https://%s:%s@github.com/dvthiriez/rvi_core.git' % (GIT_USERNAME, GIT_PASSWORD)
 MYSQL_PW = 'newpwd'
 
 
@@ -18,11 +19,14 @@ def deploy():
     _create_directory_structure_if_necessary(backend_site_folder)
     _get_latest_source(backend_source_folder, BACKEND_REPO_URL)
     _get_latest_source(core_site_folder, CORE_REPO_URL)
+    _update_and_build_rvi_core(core_site_folder, env.host)
     _update_settings(backend_source_folder, env.host)
     _update_virtualenv(backend_source_folder)
     with shell_env(PYTHONPATH=backend_source_folder):
         _update_static_files(backend_source_folder)
         _update_database(backend_source_folder)
+    _config_gunicorn_and_nginx(backend_source_folder, env.host)
+    _launch_gunicorn_and_ngninx(env.host)
  
 
 def _create_directory_structure_if_necessary(backend_site_folder):
@@ -39,6 +43,16 @@ def _get_latest_source(source_folder, repo_url):
     # rvi_core
     # current_commit = local("git log -n 1 --format=%H", capture=True)
     # run('cd %s && git reset --hard %s' % (source_folder, current_commit))
+
+
+def _update_and_build_rvi_core(core_site_folder, site_name):
+    run('cd %s && make clean && make' % (core_site_folder,))
+    backend_config_file = core_site_folder + '/backend.config'
+    sed(backend_config_file,
+        '\{ node_address, "54.172.25.254:8807" \},',
+        '\{ node_address, "%s:8807" \},' % (site_name,)
+    )
+    run('cd %s && ./scripts/setup_rvi_node.sh -n backend_node -c backend.config' % (core_site_folder,))
 
 
 def _update_settings(backend_source_folder, site_name):
@@ -61,7 +75,7 @@ def _update_virtualenv(backend_source_folder):
     virtualenv_folder = backend_source_folder + '/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
         run('virtualenv --python=python2.7 %s' % (virtualenv_folder,))
-    run('%s/bin/pip install -r %s/config/requirements/production.txt' % (
+    run('%s/bin/pip install -r %s/config/requirements/base.txt' % (
         virtualenv_folder, backend_source_folder
     ))
 
@@ -97,3 +111,26 @@ def _update_database(backend_source_folder):
     run('cd %s/web && ../../virtualenv/bin/python2.7 manage.py migrate --noinput' % (
         backend_source_folder,
     ))
+
+def _config_gunicorn_and_nginx(backend_source_folder, site_name):
+    run(('cd %s && \\'
+         'sed "s/SITENAME/%s/g" \\'
+         'deploy_tools/nginx.template.conf | sudo tee \\'
+         '/etc/nginx/sites-available/%s')
+         % (backend_source_folder, site_name, site_name)
+     )
+    run(('cd %s && \\'
+         'sudo ln -s ../sites-available/%s \\'
+         '/etc/nginx/sites-enabled/%s')
+         % (backend_source_folder, site_name, site_name)
+     )
+    run(('cd %s && \\'
+         'sed "s/SITENAME/%s/g" \\'
+         'deploy_tools/gunicorn-upstart.template.conf | sudo tee \\'
+         '/etc/init/gunicorn-%s.conf')
+         % (backend_source_folder, site_name, site_name)
+     )
+
+
+def _launch_gunicorn_and_ngninx(site_name):
+    run('sudo service nginx reload && sudo start gunicorn-%s' % (site_name,))
--- deploy_tools/provisioning_notes.md
+++ deploy_tools/provisioning_notes.md
@@ -22,11 +22,42 @@ eg, on Ubuntu:
 
     # You may be prompted to set the mysql root password at this point. If so, use 'newpwd'
     # However, if you'd like to set it to something else, just update your local machine's 
-    # fabfile.py if you plan on using it for remote deployments
+    # fabfile.py if you plan on using Fabric for remote deployment
 
     sudo apt-get install libffi-dev libssl-dev libmysqlclient-dev nginx erlang git
     sudo update-rc.d mysql defaults
     sudo pip install virtualenv
+
+
+## Usage and setup of automated deployment using Fabric:
+Proceed with the steps above to set up all packages required on your server.
+
+Then, to use the fabfile.py to perform a remote deployment, execute the following commands on your local machine,
+replacing *staging.my-domain.com* with the server you're provisioning.
+```
+$ cd rvi_backend/deploy_tools
+$ pip2 install fabric
+$ fab deploy:host=ubuntu@staging.my-domain.com
+```
+
+**Note:** If access to your server is via private key, you'll have to add the key on your local maching. e.g.
+```
+$ eval `ssh-agent -s`
+$ ssh-add ~/.ssh/ec2key.pem
+$ fab deploy:host=ubuntu@staging.my-domain.com
+```
+
+For additional details or a full walkthrough, refer to chapters 8 and 9 in Test-Driven Development with Python,
+by Harry J.W. Percival available @ http://www.obeythetestinggoat.com
+
+
+
+## Additional deployment info...
+
+## Folder structure:
+
+Assume we have a user account at /home/username
+
 
 ## Nginx Virtual Host config
 
@@ -38,49 +69,3 @@ eg, on Ubuntu:
 
 * see gunicorn-upstart.template.conf
 * replace SITENAME with, eg, staging.my-domain.com
-
-
-## Folder structure:
-
-Assume we have a user account at /home/username
-
-
-## Usage and setup:
-
-To use the fabfile.py to perform a remote deployment, execute the following commands on your local machine
-```
-$ cd rvi_backend/deploy_tools
-$ pip2 install fabric
-$ fab deploy:host=ubuntu@staging.my-domain.com
-```
-
-Then connect to the remote server and update the Ngninx config
-```
-ubuntu@server:$ cd ~/sites/staging.my-domain.com/source
-```
-```
-ubuntu@server:$ sed "s/SITENAME/staging.my-domain.com/g" \
-		deploy_tools/nginx.template.conf | sudo tee \
-		/etc/nginx/sites-available/staging.my-domain.com
-```
-```
-ubuntu@server:$ sudo ln -s ../sites-available/staging.my-domain.com \
-		/etc/nginx/sites-enabled/staging.my-domain.com
-```
-
-... and the Upstart job
-
- ```
-ubuntu@server: sed "s/SITENAME/staging.my-domain.com/g" \
-	       deploy_tools/gunicorn-upstart.template.conf | sudo tee \
-	       /etc/init/gunicorn-staging.my-domain.com.conf
-```
-
-After setting up the Nginx and Upstart scripts, restart their respective services
-```
-ubuntu@server:$ sudo service nginx reload
-ubuntu@server:$ sudo start gunicorn-staging.my-domain.com
-```
-
-For additional details or a full walkthrough, refer to chapters 8 and 9 in Test-Driven Development with Python,
-by Harry J.W. Percival available @ http://www.obeythetestinggoat.com
